f45590d13e3f0ee5cba039d64156111a407879cf
Frank <faddelia@gmail.com>
2022-08-05 16:31:52 -0500

    Add Error Handling
    Current progress for error handling

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,38 +8,50 @@ using Persistence;
 
 namespace API.Controllers
 {
-    public class ActivitiesController : BaseApiController
+  public class ActivitiesController : BaseApiController
+  {
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
-        {
-            return await Mediator.Send(new List.Query());
-        }
-
-        // acitivies/id
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
-        {
-            return await Mediator.Send(new Details.Query { Id = id });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateActivity(Activity newActivity)
-        {
-            return Ok(await Mediator.Send(new Create.Command { Activity = newActivity }));
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> EditActivity(Guid id, [FromBody] Activity activity)
-        {
-            activity.Id = id;
-            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteActivity(Guid id)
-        {
-            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
-        }
+      return await Mediator.Send(new List.Query());
     }
+
+    // acitivies/id
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetActivity(Guid id)
+    {
+      var result = await Mediator.Send(new Details.Query { Id = id });
+
+      if (result.IsSuccess && result.Value != null)
+      {
+        return Ok(result.Value);
+      }
+
+      if (result.IsSuccess && result.Value == null)
+      {
+        return NotFound();
+      }
+
+      return BadRequest(result.Error);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity(Activity newActivity)
+    {
+      return Ok(await Mediator.Send(new Create.Command { Activity = newActivity }));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, [FromBody] Activity activity)
+    {
+      activity.Id = id;
+      return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActivity(Guid id)
+    {
+      return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+    }
+  }
 }
--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -4,38 +4,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
 namespace Application.Activities
 {
-    public class Edit
+  public class Edit
+  {
+    public class Command : IRequest
     {
-        public class Command : IRequest
-        {
-            public Activity Activity { get; set; }
-        }
-
-        public class Handler : IRequestHandler<Command>
-        {
-            private readonly DataContext _context;
-            public IMapper _mapper { get; set; }
-            public Handler(DataContext context, IMapper mapper)
-            {
-                _mapper = mapper;
-                _context = context;
-            }
-
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
-            {
-                var activity = await _context.Activities.FindAsync(request.Activity.Id);
-
-                _mapper.Map(request.Activity, activity);
-
-                await _context.SaveChangesAsync();
-
-                return Unit.Value;
-            }
-        }
+      public Activity Activity { get; set; }
     }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+      public CommandValidator()
+      {
+        RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+      }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+      private readonly DataContext _context;
+      public IMapper _mapper { get; set; }
+      public Handler(DataContext context, IMapper mapper)
+      {
+        _mapper = mapper;
+        _context = context;
+      }
+
+      public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+      {
+        var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+        _mapper.Map(request.Activity, activity);
+
+        await _context.SaveChangesAsync();
+
+        return Unit.Value;
+      }
+    }
+  }
 }
