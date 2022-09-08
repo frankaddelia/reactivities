5bbf1f37ffe5bb7d91fea2421be697c03e9623dd
faddelia <faddelia@gmail.com>
2022-09-07 17:29:45 -0700

    Feature/error handling (#3)
    * Refactor API logic
    
    * Refactor Create endpoint
    
    * Refactor delete and edit endpoints
    
    * Add exception handling and middleware
    
    * Add buggy controller for testing
    
    * Current proress

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,47 +11,35 @@ namespace API.Controllers
   public class ActivitiesController : BaseApiController
   {
     [HttpGet]
-    public async Task<ActionResult<List<Activity>>> GetActivities()
+    public async Task<IActionResult> GetActivities()
     {
-      return await Mediator.Send(new List.Query());
+      return HandleResult(await Mediator.Send(new List.Query()));
     }
 
     // acitivies/id
     [HttpGet("{id}")]
     public async Task<IActionResult> GetActivity(Guid id)
     {
-      var result = await Mediator.Send(new Details.Query { Id = id });
-
-      if (result.IsSuccess && result.Value != null)
-      {
-        return Ok(result.Value);
-      }
-
-      if (result.IsSuccess && result.Value == null)
-      {
-        return NotFound();
-      }
-
-      return BadRequest(result.Error);
+      return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateActivity(Activity newActivity)
     {
-      return Ok(await Mediator.Send(new Create.Command { Activity = newActivity }));
+      return HandleResult(await Mediator.Send(new Create.Command { Activity = newActivity }));
     }
 
     [HttpPut("{id}")]
     public async Task<IActionResult> EditActivity(Guid id, [FromBody] Activity activity)
     {
       activity.Id = id;
-      return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+      return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteActivity(Guid id)
     {
-      return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+      return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
     }
   }
 }
--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Core;
 using AutoMapper;
 using Domain;
 using FluentValidation;
@@ -12,7 +13,7 @@ namespace Application.Activities
 {
   public class Edit
   {
-    public class Command : IRequest
+    public class Command : IRequest<Result<Unit>>
     {
       public Activity Activity { get; set; }
     }
@@ -25,7 +26,7 @@ namespace Application.Activities
       }
     }
 
-    public class Handler : IRequestHandler<Command>
+    public class Handler : IRequestHandler<Command, Result<Unit>>
     {
       private readonly DataContext _context;
       public IMapper _mapper { get; set; }
@@ -35,15 +36,19 @@ namespace Application.Activities
         _context = context;
       }
 
-      public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+      public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
       {
         var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
+        if (activity == null) return Result<Unit>.Failure("Cannot edit the activity since it does not exist.");
+
         _mapper.Map(request.Activity, activity);
 
-        await _context.SaveChangesAsync();
+        var result = await _context.SaveChangesAsync() > 0;
 
-        return Unit.Value;
+        if (!result) return Result<Unit>.Failure("Failed to update activity.");
+
+        return Result<Unit>.Success(Unit.Value);
       }
     }
   }
