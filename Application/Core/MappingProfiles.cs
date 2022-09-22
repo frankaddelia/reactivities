c257fa29971c6edec7bd333caeb9a8d22c24470d
Frank <faddelia@gmail.com>
2022-09-22 16:24:28 -0500

    Add auto mapper. Fix the activities to send back

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -2,16 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
 using AutoMapper;
 using Domain;
 
 namespace Application.Core
 {
-    public class MappingProfiles : Profile
+  public class MappingProfiles : Profile
+  {
+    public MappingProfiles()
     {
-        public MappingProfiles()
-        {
-            CreateMap<Activity, Activity>();
-        }
+      CreateMap<Activity, Activity>();
+      CreateMap<Activity, ActivityDto>();
     }
+  }
 }
