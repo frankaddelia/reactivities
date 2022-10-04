2580e3366df8f129fc36a35dd859f1631377dbf5
faddelia <faddelia@gmail.com>
2022-10-04 09:42:22 -0500

    Feature/api image upload (#7)
    * Current progress
    
    * Add photo and photo accessor class
    
    * Add photos property
    
    * Add migration for photos
    
    * Add Add class
    
    * Add photos controller
    
    * Add delete class
    
    * Add Delete photo endpoint
    
    * Add more handlers
    
    * Create attendee dto. Refactor to use new dto.

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -6,7 +6,9 @@ namespace Domain
   public class AppUser : IdentityUser
   {
     public string DisplayName { get; set; }
+    // public string UserName { get; set; }
     public string Bio { get; set; }
     public ICollection<ActivityAttendee> Activities { get; set; }
+    public ICollection<Photo> Photos { get; set; }
   }
 }
