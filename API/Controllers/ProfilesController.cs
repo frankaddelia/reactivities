6627a8716e72391457000975329593b02db55627
Frank <faddelia@gmail.com>
2022-10-06 10:10:13 -0500

    Add update profile endpoint

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Profiles;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -13,6 +14,13 @@ namespace API.Controllers
     public async Task<IActionResult> GetProfile(string username)
     {
       return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+    }
+
+    [Authorize]
+    [HttpPut]
+    public async Task<IActionResult> UpdateProfile([FromBody] Profile profile)
+    {
+      return HandleResult(await Mediator.Send(new Update.Command { Profile = profile }));
     }
   }
 }
