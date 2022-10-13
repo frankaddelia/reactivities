dcf88e2b8fd32a9fa07ee45e944fe04e008c099e
Frank <faddelia@gmail.com>
2022-10-13 17:02:33 -0500

    Fix mappings that need the following info

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Domain;
@@ -20,8 +21,10 @@ namespace Application.Activities
     {
       private readonly DataContext _context;
       private readonly IMapper _mapper;
-      public Handler(DataContext context, IMapper mapper)
+      private readonly IUserAccessor _userAcccessor;
+      public Handler(DataContext context, IMapper mapper, IUserAccessor userAcccessor)
       {
+        _userAcccessor = userAcccessor;
         _context = context;
         _mapper = mapper;
       }
@@ -29,7 +32,9 @@ namespace Application.Activities
       public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
       {
         var activities = await _context.Activities
-          .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+          .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider,
+            new { currentUsername = _userAcccessor.GetUsername() }
+          )
           .ToListAsync(cancellationToken);
 
         return Result<List<ActivityDto>>.Success(activities);
