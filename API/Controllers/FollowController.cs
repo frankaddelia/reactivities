949bb0b6436e2a7451010519e49d0c0930cbc277
Frank <faddelia@gmail.com>
2022-10-13 14:29:13 -0500

    Add following and following/followers count

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -7,12 +7,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class FollowController : BaseApiController
+  public class FollowController : BaseApiController
+  {
+    [HttpPost("{username}")]
+    public async Task<IActionResult> Follow(string username)
     {
-        [HttpPost("{username}")]
-        public async Task<IActionResult> Follow(string username)
-        {
-            return HandleResult(await Mediator.Send(new FollowToggle.Command{TargetUsername = username}));
-        }
+      return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
     }
+
+    [HttpGet("{username}")]
+    public async Task<IActionResult> GetFollowings(string username, string predicate)
+    {
+      return HandleResult(
+            await Mediator.Send(
+                new List.Query { Username = username, Predicate = predicate }
+            )
+        );
+    }
+  }
 }
